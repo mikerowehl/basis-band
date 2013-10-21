57b7b0f1df80d71711485a92d88f83cf197e4f50
Mike Rowehl <mikerowehl@gmail.com>
2013-10-21 16:52:31 -0700

    Adds a samples_by_minute function to the api model.

--- lib/basis-band/api-response-model.rb
+++ lib/basis-band/api-response-model.rb
@@ -1,11 +1,25 @@
+require 'time'
 require 'json'
 
 class ApiResponseModel
   def initialize(raw_text)
     @json = JSON.parse(raw_text)
+    # Use the local timezone intentionally
+    @starttime = Time.at(@json['starttime'])
   end
 
-  def heartrate_samples
-    @json["metrics"]["heartrate"]["values"].length
+  def hash_for_minute(min)
+    t = @starttime + (min * 60)
+    res = {'t' => t.strftime("%Y/%m/%d %H:%M:%S")}
+    for m in @json["metrics"].keys
+      res[m] = @json["metrics"][m]["values"][min]
+    end
+    res
+  end
+
+  def samples_by_minute
+    (0...1440).map { |x|
+      hash_for_minute(x)
+    }
   end
 end
--- test/test_api-response-model.rb
+++ test/test_api-response-model.rb
@@ -6,7 +6,14 @@ class ApiResponseModelTest < Test::Unit::TestCase
     test_fn = File.expand_path('../2013-10-01.json', __FILE__)
     raw = File.read(test_fn)
     m = ApiResponseModel.new(raw)
-    puts m.heartrate_samples
+    minutes = m.samples_by_minute
+    assert minutes.length == 1440
+    min_first = minutes.first
+    assert min_first['t'] == "2013/10/01 00:00:00"
+    assert min_first["air_temp"] == 80.6
+    min_last = minutes.last
+    assert min_last['t'] == "2013/10/01 23:59:00"
+    assert min_last["air_temp"] == 81.5
   end
 end
 
