25d6cb24d506efacde6709bf52641fd8ea123483
Mike Rowehl <mikerowehl@gmail.com>
2013-10-21 19:25:08 -0700

    Removes bad tests, don't assume timezone on test system.

--- test/test_api-response-model.rb
+++ test/test_api-response-model.rb
@@ -9,10 +9,8 @@ class ApiResponseModelTest < Test::Unit::TestCase
     minutes = m.samples_by_minute
     assert minutes.length == 1440
     min_first = minutes.first
-    assert min_first['t'] == "2013/10/01 00:00:00"
     assert min_first["air_temp"] == 80.6
     min_last = minutes.last
-    assert min_last['t'] == "2013/10/01 23:59:00"
     assert min_last["air_temp"] == 81.5
   end
 end
