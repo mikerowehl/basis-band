10721d88b5c6918ebb57e4edd7ca6fe50c055c0f
Mike Rowehl <mikerowehl@gmail.com>
2013-10-30 14:43:37 -0700

    Adds some basic filesystem caching

--- lib/basis-band.rb
+++ lib/basis-band.rb
@@ -4,12 +4,48 @@ require 'basis-band/api-auth'
 require 'basis-band/api-response-model'
 
 class BasisBand
+  @cache_dir = nil
+
   def initialize(userid)
     @userid = userid
   end
 
+  def set_cache_dir(dir)
+    @cache_dir = dir
+  end
+
   def data_for_day(date)
+    r = cached_value_for_day(date)
+    if !r
+      r = fetch_value_for_day(date)
+    end
+    r
+  end
+
+  def cache_filename(date)
+    File.join(@cache_dir, date + ".json")
+  end
+
+  def cached_value_for_day(date)
+    raw = nil
+    begin
+      File.open(cache_filename(date), "r") { |f|
+        raw = f.read
+      }
+    rescue
+      # ignore exception
+    end
+    raw
+  end
+
+  def fetch_value_for_day(date)
     f = ApiFetch.new()
-    f.get_day(@userid, date)
+    raw = f.get_day(@userid, date)
+    if raw && @cache_dir
+      File.open(cache_filename(date), "w") { |f|
+        f.write(raw)
+      }
+    end
+    raw
   end
 end
